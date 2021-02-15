63c656b08cc06305a164ed437407a36d511fc3ab
billdelvin <billdelvin6@gmail.com>
2021-02-15 12:20:06 +0700

    create controller with func and class

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import Head from 'next/head';
 import Navbar from '../components/navbar';
 import Sidebar from '../components/sidebar';
@@ -6,43 +6,10 @@ import Carousel from '../components/carousel';
 import MovieList from '../components/movieList';
 import Footer from '../components/footer';
 
-const MOVIE_DATA = [
- {
-  id: '1',
-  name: 'The Shawshank Redemption',
-  releaseYear: 1994,
-  description:
-   'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-  rating: 4.8,
-  genre: 'drama',
-  image:
-   'https://m.media-amazon.com/images/M/MV5BNjQ2NDA3MDcxMF5BMl5BanBnXkFtZTgwMjE5NTU0NzE@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg',
- },
- {
-  id: '2',
-  name: 'The Dark Knight',
-  releaseYear: 2008,
-  description:
-   'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-  rating: 4.7,
-  genre: 'action, crime, drama',
-  image:
-   'https://img.cinemablend.com/filter:scale/quill/c/3/8/0/f/4/c380f4f12cfeec19f0c40c6f57db188f2f98cca8.jpg?mw=600',
- },
- {
-  id: '3',
-  name: 'Lord of the Rings',
-  releaseYear: 2004,
-  description:
-   'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
-  rating: 4.9,
-  genre: 'adventure, drama, fantasy',
-  image:
-   'https://img.cinemablend.com/filter:scale/quill/0/f/5/2/a/6/0f52a6843a25c1a5c1f9a0c00548cad9e1d912e2.jpg?mw=600',
- },
-];
+import { getMovies } from '../actions';
 
-const Home = () => {
+const Home = (props) => {
+ const { movies } = props;
  return (
   <div>
    <Head>
@@ -76,7 +43,7 @@ const Home = () => {
      <Sidebar />
      <div className='col-lg-9'>
       <Carousel />
-      <MovieList movies={MOVIE_DATA} />
+      <MovieList movies={movies} />
      </div>
     </div>
    </div>
@@ -84,5 +51,90 @@ const Home = () => {
   </div>
  );
 };
+
+Home.getInitialProps = async () => {
+ const movies = await getMovies();
+ return { movies };
+};
+
+// class Home extends Component {
+//  static async getInitialProps() {
+//   const movies = await getMovies();
+//   return { movies };
+//  }
+
+//  // constructor(props) {
+//  //  super(props);
+//  //  this.state = {
+//  //   movies: [],
+//  //   error: '',
+//  //  };
+//  // }
+
+//  // it's only called in client browser
+//  // componentDidMount() {
+//  //  getMovies()
+//  //   .then((movies) => {
+//  //    this.setState({ movies });
+//  //   })
+//  //   .catch((error) => {
+//  //    this.setState({ error });
+//  //   });
+//  // }
+
+//  // state = {
+//  //  movies: [],
+//  // };
+
+//  // called only once when component is mounted!
+//  // async componentDidMount() {
+//  //  const movies = await getMovies();
+//  //  this.setState({ movies });
+//  // }
+
+//  render() {
+//   const { movies } = this.props;
+//   return (
+//    <div>
+//     <Head>
+//      <title>Home</title>
+//      <link
+//       rel='stylesheet'
+//       href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+//       integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T'
+//       crossOrigin='anonymous'
+//      />
+//      <script
+//       src='https://code.jquery.com/jquery-3.3.1.slim.min.js'
+//       integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo'
+//       crossOrigin='anonymous'
+//      ></script>
+//      <script
+//       src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'
+//       integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1'
+//       crossOrigin='anonymous'
+//      ></script>
+//      <script
+//       src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'
+//       integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM'
+//       crossOrigin='anonymous'
+//      ></script>
+//     </Head>
+
+//     <Navbar />
+//     <div className='container'>
+//      <div className='row'>
+//       <Sidebar />
+//       <div className='col-lg-9'>
+//        <Carousel />
+//        <MovieList movies={movies} />
+//       </div>
+//      </div>
+//     </div>
+//     <Footer />
+//    </div>
+//   );
+//  }
+// }
 
 export default Home;
