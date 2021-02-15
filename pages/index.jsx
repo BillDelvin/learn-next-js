276a872dcd988980be663ae83bab8bd47958eb14
billdelvin <billdelvin6@gmail.com>
2021-02-15 14:52:01 +0700

    change file type

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,7 @@
-import React, { useState, useEffect, Component } from 'react';
-import Head from 'next/head';
-import Navbar from '../components/navbar';
+import React from 'react';
 import Sidebar from '../components/sidebar';
 import Carousel from '../components/carousel';
 import MovieList from '../components/movieList';
-import Footer from '../components/footer';
 
 import { getMovies } from '../actions';
 
@@ -12,42 +9,15 @@ const Home = (props) => {
  const { movies } = props;
  return (
   <div>
-   <Head>
-    <title>Home</title>
-    <link
-     rel='stylesheet'
-     href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
-     integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T'
-     crossOrigin='anonymous'
-    />
-    <script
-     src='https://code.jquery.com/jquery-3.3.1.slim.min.js'
-     integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo'
-     crossOrigin='anonymous'
-    ></script>
-    <script
-     src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'
-     integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1'
-     crossOrigin='anonymous'
-    ></script>
-    <script
-     src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'
-     integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM'
-     crossOrigin='anonymous'
-    ></script>
-   </Head>
-
-   <Navbar />
    <div className='container'>
     <div className='row'>
      <Sidebar />
      <div className='col-lg-9'>
       <Carousel />
-      <MovieList movies={movies} />
+      <MovieList movies={movies || []} />
      </div>
     </div>
    </div>
-   <Footer />
   </div>
  );
 };
