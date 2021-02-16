3dae65de9f308b47e970e75d2be7b625d440024e
billdelvin <billdelvin6@gmail.com>
2021-02-16 10:37:16 +0700

    dispay data from category

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+ const { categories } = props;
  return (
   <div className='col-lg-3'>
    <h1 className='my-4'>Shop Name</h1>
    <div className='list-group'>
-    <a href='#' className='list-group-item'>
-     Category 1
-    </a>
-    <a href='#' className='list-group-item'>
-     Category 2
-    </a>
-    <a href='#' className='list-group-item'>
-     Category 3
-    </a>
+    {categories.map((c) => (
+     <a href='#' className='list-group-item' key={c.id}>
+      {c.name}
+     </a>
+    ))}
    </div>
   </div>
  );
