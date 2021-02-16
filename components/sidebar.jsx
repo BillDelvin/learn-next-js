d37d36a1b10013f7d830e3c90e1547f03cb118e8
billdelvin <billdelvin6@gmail.com>
2021-02-16 15:19:44 +0700

    addding modal and form component

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Modal from './modal';
+import Form from './form';
+
 const Sidebar = (props) => {
  const { categories } = props;
  return (
-  <div className='col-lg-3'>
+  <div className='col-lg-3 mt-3'>
+   <Modal>
+    <Form />
+   </Modal>
    <h1 className='my-4'>Shop Name</h1>
    <div className='list-group'>
     {categories.map((c) => (
