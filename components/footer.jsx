202ba152cbc5c447c8036342c29c3e2d24b234b2
billdelvin <billdelvin6@gmail.com>
2021-03-10 19:25:15 +0700

    change css class

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
  return (
-  <footer className='py-5 bg-dark'>
-   <div className='container'>
-    <p className='m-0 text-center text-white'>Copyright &copy; Your Website 2019</p>
+  <footer className="py-3 bg-dark">
+   <div className="container">
+    <p className="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
    </div>
   </footer>
  );
