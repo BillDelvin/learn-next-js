f16d7c98363da7cc37993bed05ef0421c07b063f
billdelvin <billdelvin6@gmail.com>
2021-03-15 10:51:54 +0700

    clean code

--- components/form.jsx
+++ components/form.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 const Form = (props) => {
+ const { handleSubmitForm } = props;
  const defaultData = {
   name: '',
   description: '',
@@ -38,88 +39,88 @@ const Form = (props) => {
  };
 
  const onSubmit = () => {
-  props.handleSubmitForm({ ...form });
+  handleSubmitForm(form);
  };
 
  return (
-  <form>
-   <div className='form-group'>
-    <label htmlFor='name'>Name</label>
+  <form className="pb-50">
+   <div className="form-group">
+    <label htmlFor="name">Name</label>
     <input
-     type='text'
-     className='form-control'
-     id='name'
-     name='name'
+     type="text"
+     className="form-control"
+     id="name"
+     name="name"
      defaultValue={form.name}
      onChange={handleChange}
     />
    </div>
-   <div className='form-group'>
-    <label htmlFor='description'>Description</label>
+   <div className="form-group">
+    <label htmlFor="description">Description</label>
     <input
-     type='text'
-     className='form-control'
-     id='description'
-     name='description'
+     type="text"
+     className="form-control"
+     id="description"
+     name="description"
      defaultValue={form.description}
      onChange={handleChange}
     />
    </div>
-   <div className='form-group'>
-    <label htmlFor='description'>Rating</label>
+   <div className="form-group">
+    <label htmlFor="description">Rating</label>
     <input
-     type='number'
-     max='5'
-     min='0'
-     className='form-control'
-     id='rating'
-     name='rating'
-     placeholder='3'
+     type="number"
+     max="5"
+     min="0"
+     className="form-control"
+     id="rating"
+     name="rating"
+     placeholder="3"
      defaultValue={form.rating}
      onChange={handleChange}
     />
-    <small id='emailHelp' className='form-text text-muted'>
+    <small id="emailHelp" className="form-text text-muted">
      Max: 5, Min: 0{' '}
     </small>
    </div>
-   <div className='form-group'>
-    <label htmlFor='image'>Image</label>
+   <div className="form-group">
+    <label htmlFor="image">Image</label>
     <input
-     type='text'
-     className='form-control'
-     id='image'
-     name='image'
-     placeholder='http://.....'
+     type="text"
+     className="form-control"
+     id="image"
+     name="image"
+     placeholder="http://....."
      defaultValue={form.image}
      onChange={handleChange}
     />
    </div>
-   <div className='form-group'>
-    <label htmlFor='cover'>Cover</label>
+   <div className="form-group">
+    <label htmlFor="cover">Cover</label>
     <input
-     type='text'
-     className='form-control'
-     id='cover'
-     name='cover'
-     placeholder='http://......'
+     type="text"
+     className="form-control"
+     id="cover"
+     name="cover"
+     placeholder="http://......"
      defaultValue={form.cover}
      onChange={handleChange}
     />
    </div>
-   <div className='form-group'>
-    <label htmlFor='longDesc'>Long Description</label>
+   <div className="form-group">
+    <label htmlFor="longDesc">Long Description</label>
     <textarea
-     className='form-control'
-     id='longDesc'
-     name='longDesc'
-     rows='3'
+     className="form-control"
+     id="longDesc"
+     name="longDesc"
+     rows="3"
      defaultValue={form.longDesc}
      onChange={handleChange}
     ></textarea>
    </div>
-   <div className='form-group'>
-    <label htmlFor='genre'>Genre</label>
-    <select multiple className='form-control' id='genre' name='genre' onChange={handleGenre}>
+   <div className="form-group">
+    <label htmlFor="genre">Genre</label>
+    <select multiple className="form-control" id="genre" name="genre" onChange={handleGenre}>
      <option>drama</option>
      <option>music</option>
      <option>adventure</option>
@@ -127,7 +128,7 @@ const Form = (props) => {
      <option>action</option>
     </select>
    </div>
-   <button type='button' onClick={onSubmit} className='btn btn-primary'>
+   <button type="button" onClick={onSubmit} className="btn btn-primary">
     Create
    </button>
   </form>
